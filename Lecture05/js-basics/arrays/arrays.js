6023cd020816d8a3f3efeced97f0084e0217c12a
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-08-30 17:07:23 +0530

    js: arrays

--- Lecture05/js-basics/arrays/arrays.js
+++ Lecture05/js-basics/arrays/arrays.js
@@ -14,3 +14,34 @@ for(let val of arr) {
 for(let index of arr) {
     console.log(index);
 }
+//Add and remove at last index
+let fruits = ["apple","mango"];
+console.log(fruits);
+fruits.push("melon");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+//Add and remove at first index
+let cars = ["Camary","Baleno","Ciaz","Nexon"];
+console.log(cars);
+cars.shift();
+console.log(cars);
+cars.unshift("Swift");
+console.log(cars);
+
+//Arrays: Data Changing
+let gadgets = ["smartphone","tv","washing machine","refrigerator"];
+
+//slice method is non destructive
+console.log(gadgets.slice(2));
+console.log(gadgets.slice(2,3));
+
+//splice method mutates the array
+console.log(gadgets.splice(2));
+console.log(gadgets);
+
+// splice can take another value as argument to specify length it will slice
+gadgets = ["smartphone","tv","washing machine","refrigerator"];
+//taking "washing machine" out
+console.log(gadgets.splice(2,1));
+console.log(gadgets);
